e4b74c2c1ee64e05b0afac19ed911f357eabc25a
tsheporamantso <tshepo.ramantso@outlook.com>
2024-09-05 17:48:45 +0200

    Import error handler middleware

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const mainRoute = require('./routes/main');
 const notFoundMiddleware = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
 require('dotenv').config();
 
 const app = express();
 
 app.use(express.static('./public'));
+app.use(express.json());
 
 app.use('/api/v1', mainRoute);
 app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
 const { PORT } = process.env || 3000;
 const start = () => {
   try {
