0b151b60d8fd671006b34f77bbfe8c08304536e1
tsheporamantso <tshepo.ramantso@outlook.com>
2024-09-05 17:25:15 +0200

    Import and use not found middleware

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mainRoute = require('./routes/main');
+const notFoundMiddleware = require('./middleware/not-found');
 require('dotenv').config();
 
 const app = express();
@@ -7,7 +8,7 @@ const app = express();
 app.use(express.static('./public'));
 
 app.use('/api/v1', mainRoute);
-
+app.use(notFoundMiddleware);
 const { PORT } = process.env || 3000;
 const start = () => {
   try {
