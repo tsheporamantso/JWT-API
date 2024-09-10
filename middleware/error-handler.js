53fc0ef2055aed9083764a7656827f48dd96c95b
tsheporamantso <tshepo.ramantso@outlook.com>
2024-09-10 18:27:44 +0200

    Corrected typos.

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,13 +1,13 @@
-const { StatusCode } = require('http-status-codes');
+const { StatusCodes } = require('http-status-codes');
 const { CustomeAPIError } = require('../errors');
 
 const errorHandleMiddleware = async (err, req, res, next) => {
   if (err instanceof CustomeAPIError) {
     next();
-    return res.status(err.statusCode).json({ msg: err.message });
+    return res.status(err.StatusCodes).json({ msg: err.message });
   }
   return res
-    .status(StatusCode.INTERNAL_SERVER_ERROR)
+    .status(StatusCodes.INTERNAL_SERVER_ERROR)
     .send('Something went wrong try again');
 };
 
