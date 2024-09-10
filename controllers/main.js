aceffac8144d47b36469d2ea60a249be7b0a20fd
tsheporamantso <tshepo.ramantso@outlook.com>
2024-09-10 18:26:32 +0200

    Code refactoring

--- controllers/main.js
+++ controllers/main.js
@@ -4,21 +4,21 @@ const { BadRequestError } = require('../errors');
 
 const login = async (req, res) => {
   const { username, password } = req.body;
-  if (username || password) {
-    const id = new Date().getDate();
 
-    const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
-      expiresIn: process.env.JWT_TIMELINE,
-    });
-    res.status(StatusCodes.OK).json({ msg: 'user created', token });
-  } else {
+  if (!username || !password) {
     throw new BadRequestError('Please provide username and password');
   }
+  const id = new Date().getDate();
+
+  const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_TIMELINE,
+  });
+  res.status(StatusCodes.OK).json({ msg: 'user created', token });
 };
 const dashboard = async (req, res) => {
   const luckyNumber = Math.floor(Math.random() * 100 + 1);
   res.status(StatusCodes.OK).json({
-    msg: 'Hello Dumi',
+    msg: `Hello ${req.user.username}`,
     secret: `Here is your authorized Data, and your lucky number is ${luckyNumber}`,
   });
 };
