0ae1369a132992bbf0d550096da4a63f2e624f32
tsheporamantso <tshepo.ramantso@outlook.com>
2024-09-10 15:27:14 +0200

    Add jwt timeline

--- controllers/main.js
+++ controllers/main.js
@@ -8,7 +8,7 @@ const login = async (req, res) => {
     const id = new Date().getDate();
 
     const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
-      expiresIn: '30d',
+      expiresIn: process.env.JWT_TIMELINE,
     });
     res.status(StatusCodes.OK).json({ msg: 'user created', token });
   } else {
@@ -17,12 +17,10 @@ const login = async (req, res) => {
 };
 const dashboard = async (req, res) => {
   const luckyNumber = Math.floor(Math.random() * 100 + 1);
-  res
-    .status(StatusCodes.OK)
-    .json({
-      msg: 'Hello Dumi',
-      secret: `Here is your Auth Data, and your lucky number is ${luckyNumber}`,
-    });
+  res.status(StatusCodes.OK).json({
+    msg: 'Hello Dumi',
+    secret: `Here is your Auth Data, and your lucky number is ${luckyNumber}`,
+  });
 };
 
 module.exports = {
